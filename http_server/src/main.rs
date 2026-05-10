6b94b139d87132786b1918a78a7eaead477e32e6
Antonis Zalonis <antonis.zalonis@gmail.com>
2026-05-10 14:40:54 +0300

    Enum and Option enum in Rust

--- http_server/src/main.rs
+++ http_server/src/main.rs
@@ -19,3 +19,26 @@ impl Server {
     }
 }
 
+struct Request {
+    path: String,
+    query_string: Option<String>,
+    method: Method,
+}
+
+enum Method {
+    GET,
+    DELETE,
+    POST,
+    PUT,
+    HEAD,
+    CONNECT,
+    OPTIONS,
+    TRACE,
+    PATCH,
+}
+
+/* 
+GET /user?id=10 HTTP/1.1\r\n
+HEADERS \r\n
+BODY
+*/
