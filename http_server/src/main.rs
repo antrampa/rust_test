72359f116e08f9591942c69e6a187384d90888d4
Antonis Zalonis <antonis.zalonis@gmail.com>
2026-05-10 14:15:49 +0300

    struct and impl in Rust

--- http_server/src/main.rs
+++ http_server/src/main.rs
@@ -1,28 +1,21 @@
 fn main() {
-    let string = String::from("127.0.0.1:8080");
-    let string_slice = &string[10..];
-
-    dbg!(&string);    
-    dbg!(string_slice);
-
-    
-    // let server = Server::new("127.0.0.1:8080");
-    // server.run();
+    let server = Server::new("127.0.0.1:8080".to_string());
+    server.run();
 }
 
-// struct Server {
-//     addr: String,
-// }
+struct Server {
+    addr: String,
+}
 
-// impl Server {
-//     fn new(addr: String) -> Self {
-//         Server {
-//             addr
-//         }
-//     }
+impl Server {
+    fn new(addr: String) -> Self {
+        Server {
+            addr
+        }
+    }
 
-//     fn run(self) {
-
-//     }
-// }
+    fn run(self) {
+        println!("Listening on {}", self.addr);
+    }
+}
 
