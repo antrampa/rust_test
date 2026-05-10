401477d717bc15802fcdb16f88d269b85b8951e4
Antonis Zalonis <antonis.zalonis@gmail.com>
2026-05-10 16:53:19 +0300

    TcpListener in rust

--- http_server/src/server.rs
+++ http_server/src/server.rs
@@ -1,15 +1,17 @@
+ use std::net::TcpListener;
+
  pub struct Server {
     addr: String,
 }
 
 impl Server {
     pub fn new(addr: String) -> Self {
-        Server {
-            addr
-        }
+        Self { addr }
     }
 
     pub fn run(self) {
         println!("Listening on {}", self.addr);
+
+        let listener = TcpListener::bind(&self.addr).unwrap();
     }
 }
