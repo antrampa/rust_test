a648f1eda7d5330e6a95daf57689a9cfcb64c219
Antonis Zalonis <antonis.zalonis@gmail.com>
2026-05-10 14:51:21 +0300

    modules in rust

--- http_server/src/main.rs
+++ http_server/src/main.rs
@@ -1,41 +1,54 @@
+use server::Server;
+use http::request::Request;
+
 fn main() {
     let server = Server::new("127.0.0.1:8080".to_string());
     server.run();
 }
 
-struct Server {
-    addr: String,
-}
+mod server {
+    pub struct Server {
+        addr: String,
+    }
 
-impl Server {
-    fn new(addr: String) -> Self {
-        Server {
-            addr
+    impl Server {
+        pub fn new(addr: String) -> Self {
+            Server {
+                addr
+            }
+        }
+
+        pub fn run(self) {
+            println!("Listening on {}", self.addr);
         }
     }
+}
 
-    fn run(self) {
-        println!("Listening on {}", self.addr);
+mod http {
+    pub mod request {
+        use super::method::Method;
+        pub struct Request {
+            path: String,
+            query_string: Option<String>,
+            method: Method,
+        }
+    }
+    
+    pub mod method {
+        pub enum Method {
+            GET,
+            DELETE,
+            POST,
+            PUT,
+            HEAD,
+            CONNECT,
+            OPTIONS,
+            TRACE,
+            PATCH,
+        }
     }
 }
 
-struct Request {
-    path: String,
-    query_string: Option<String>,
-    method: Method,
-}
-
-enum Method {
-    GET,
-    DELETE,
-    POST,
-    PUT,
-    HEAD,
-    CONNECT,
-    OPTIONS,
-    TRACE,
-    PATCH,
-}
 
 /* 
 GET /user?id=10 HTTP/1.1\r\n
