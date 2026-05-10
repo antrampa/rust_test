9c3cdb866517286eff23ec75cd2214c8f3c4c7e5
Antonis Zalonis <antonis.zalonis@gmail.com>
2026-05-10 15:50:51 +0300

    modules in seperate files and folders in rust

--- http_server/src/main.rs
+++ http_server/src/main.rs
@@ -1,52 +1,13 @@
-use server::Server;
+use http::method::Method;
 use http::request::Request;
+use server::Server;
+
+mod http;
+mod server;
 
 fn main() {
     let server = Server::new("127.0.0.1:8080".to_string());
     server.run();
-}
-
-mod server {
-    pub struct Server {
-        addr: String,
-    }
-
-    impl Server {
-        pub fn new(addr: String) -> Self {
-            Server {
-                addr
-            }
-        }
-
-        pub fn run(self) {
-            println!("Listening on {}", self.addr);
-        }
-    }
-}
-
-mod http {
-    pub mod request {
-        use super::method::Method;
-        pub struct Request {
-            path: String,
-            query_string: Option<String>,
-            method: Method,
-        }
-    }
-    
-    pub mod method {
-        pub enum Method {
-            GET,
-            DELETE,
-            POST,
-            PUT,
-            HEAD,
-            CONNECT,
-            OPTIONS,
-            TRACE,
-            PATCH,
-        }
-    }
 }
 
 
