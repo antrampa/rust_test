fa7fa6f15adb0b69b1c55ce0b461dbded1386a49
Antonis Zalonis <antonis.zalonis@gmail.com>
2026-05-16 00:20:04 +0300

    Response 200 OK

--- http_server/src/http/response.rs
+++ http_server/src/http/response.rs
@@ -1,5 +1,5 @@
-// use std::net::TcpStream;
-// use std::io::{Write, Result as IoResult};
+use std::net::TcpStream;
+use std::io::{Write, Result as IoResult};
 use std::fmt::{Display, Formatter, Result as FmtResult};
 
 use super::StatusCode;
@@ -15,35 +15,18 @@ impl Response {
         Response { status_code, body}
     }
 
-    // pub fn send(&self, stream: &mut TcpStream) -> IoResult<()> {
-    //     let body = match &self.body {
-    //         Some(b) => b,
-    //         None => "",
-    //     };
-
-    //     write!(
-    //         f, 
-    //         "HTTP/1.1 {} {}\r\n\r\n{}", 
-    //         self.status_code, 
-    //         self.status_code.reason_phrase(),
-    //         body
-    //     )
-    // }
-}
-
-impl Display for Response {
-    fn fmt(&self, f: &mut Formatter) -> FmtResult {
-       let body = match &self.body {
+    pub fn send(&self, stream: &mut TcpStream) -> IoResult<()> {
+        let body = match &self.body {
             Some(b) => b,
             None => "",
         };
 
         write!(
-            f, 
+            stream, 
             "HTTP/1.1 {} {}\r\n\r\n{}", 
             self.status_code, 
             self.status_code.reason_phrase(),
             body
-        ) 
+        )
     }
 }
