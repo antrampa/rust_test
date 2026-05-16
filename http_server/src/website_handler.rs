868ef70a765072ffe3aa4c631befebed0a2e641d
Antonis Zalonis <antonis.zalonis@gmail.com>
2026-05-16 16:22:44 +0300

    Request  WebsiteHandler,  implementation of Handler

---
+++ http_server/src/website_handler.rs
@@ -0,0 +1,13 @@
+use super::http::{Request, Response, StatusCode, Method};
+use super::server::Handler;
+
+pub struct WebsiteHandler;
+
+impl Handler for WebsiteHandler {
+    fn handle_request(&mut self, request: &Request) -> Response {
+        //Response::new(StatusCode::Ok, Some("<h1>TEST</h1>".to_string()))
+        match request.method() {
+            _ => Response::new(StatusCode::NotFound, None)
+        }
+    }
+}
