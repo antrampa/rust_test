553cc768603658ef76baeadd8f0f5db7b0a8373a
Antonis Zalonis <antonis.zalonis@gmail.com>
2026-05-19 23:31:48 +0300

    Rust copy arguments

--- rust_copy/src/main.rs
+++ rust_copy/src/main.rs
@@ -1,3 +1,13 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args : Vec<String> = env::args().collect();
+    let argc = args.len();
+    if argc < 3 {
+        println!("Give source and destination files: rust_copy <copy from> <copy to>");
+        return;
+    }
+    println!("argc: {}", argc);
+    println!("args: {:?}", args);
+    println!("Done!");
 }
