73dc4b9d0a85cb272507eca519581a06ff1991a5
Antonis Zalonis <antonis.zalonis@gmail.com>
2026-05-23 19:35:09 +0300

    copy args

--- rust_copy/src/main.rs
+++ rust_copy/src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::fs;
 
 fn main() {
     let args : Vec<String> = env::args().collect();
@@ -7,7 +8,22 @@ fn main() {
         println!("Give source and destination files: rust_copy <copy from> <copy to>");
         return;
     }
+
+
     println!("argc: {}", argc);
     println!("args: {:?}", args);
-    println!("Done!");
+
+    let source_file = &args[1];
+    let destination_file = &args[2];
+    
+    println!("source_file: {}", source_file);
+    println!("destination_file: {}", destination_file);
+
+    let content = fs::read_to_string(source_file)
+        .expect("Cannot read the file");
+
+    println!("File content: {}", content);
+    
+
+    println!("Copy Done!");
 }
